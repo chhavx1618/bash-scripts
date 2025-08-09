28b2cb109b13e80fab40135450cc7d326503a1ba
chhavx1618 <chhavi.cw.1618@gmail.com>
2025-08-09 19:29:58 +0530

    redeploy script updated

--- reference-snippets/aws-ec2-redeploy.sh
+++ reference-snippets/aws-ec2-redeploy.sh
@@ -1,2 +1,23 @@
 echo "redeploy applications running with pm2"
 
+
+echo "stop pm2 app"
+
+pm2 stop {$app_name}
+
+echo "pull latest changes from remote repository"
+
+git pull --rebase
+
+
+echo "check for node js or reactjs, choose npm run build or install etc. dynbamically"
+
+
+echo "restart pm2 app"
+
+pm2 restart {$app_name}
+
+echo "all done!"
+
+
+
