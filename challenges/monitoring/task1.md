65466f558de13e7be66234ab0679177862715637
chhavx1618 <chhavi.cw.1618@gmail.com>
2025-08-09 20:08:56 +0530

    task1 - monitoring script

---
+++ challenges/monitoring/task1.md
@@ -0,0 +1,1 @@
+Write a simple script for monitoring a list of web servers for internal errors (5XX). Add email alerts for the same.
---
+++ challenges/monitoring/task1.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+
+# log monitor script with email alerts
+
+#allow taking log files as input, other wise check the files directly
+
+
+if [ $# -eq 0 ]; then
+	echo "usage: $0 <log_file_path>"
+	exit 1
+fi
+
+
+LOG_FILE="$1"
+
+RECIPIENT_EMAIL="chhavi.cw.1618@gmail.com"
+
+if [ ! -f "$LOG_FILE" ]; then 
+	echo "erorr: file not found"
+	exit 1
+fi
+
+echo "monitoringgggg brr brrrrr................"
+
+
+
+tail -f "$LOG_FILE" | while read -r line; do 
+	if echo "$line" | grep -q " 500 "; then
+		echo "500 found oh no"
+		echo "in this $line" | mail -s "500 EERROR ALLLERT AHHH" "$RECIPIENT_EMAIL"
+	fi
+done
+
+
+
